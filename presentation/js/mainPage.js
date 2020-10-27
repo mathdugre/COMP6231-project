3770bb22824ac0eb68db6c550f82d1800c8c3fb0
Oz <frost57@gmail.com>
2020-10-27 17:55:40 -0400

    subscribe/unsubscribe front end

--- presentation/js/mainPage.js
+++ presentation/js/mainPage.js
@@ -34,3 +34,44 @@ function viewPosts() {
     document.getElementById('centerContent').innerHTML = http;
     document.getElementById("postForm").classList.add("d-none");
 }
+
+function viewUsers() {
+
+    var http = '<ul class="list-group">';
+
+    for(i = 0 ; i < 20 ; i++){
+
+        if(i < 10) {
+            http +=
+                '   <li class="list-group-item d-flex justify-content-between align-items-center">\n' +
+                '       @Username\n' +
+                '       <button onclick="changeSubButton(this)" class ="btn btn-success">Subscribe</button>\n' +
+                '   </li>';
+        } else {
+
+            http +=
+                '   <li class="list-group-item d-flex justify-content-between align-items-center">\n' +
+                '       @Username\n' +
+                '       <button onclick="changeSubButton(this)" class ="btn btn-danger">Unsubscribe</button>\n' +
+                '   </li>';
+        }
+    }
+
+    http += '</ul>'
+    document.getElementById('centerContent').innerHTML = http;
+    document.getElementById("postForm").classList.add("d-none");
+}
+
+function changeSubButton(source){
+
+    if(source.classList.contains("btn-danger")) {
+        source.classList.remove("btn-danger");
+        source.classList.add("btn-success");
+        source.innerHTML = "Subscribe";
+    } else {
+        source.classList.remove("btn-success");
+        source.classList.add("btn-danger");
+        source.innerHTML = "Unsubscribe";
+    }
+}
+
