a434d5dcddb0c7760ba54d0bbcc2fd1f95c47a1b
liuke0712 <55297033+liuke0712@users.noreply.github.com>
2020-10-31 13:30:19 -0400

    Update import_example.py

--- datasets/import_example.py
+++ datasets/import_example.py
@@ -4,10 +4,11 @@ import json
 
 class RedisTT(object):
 
+
     def __init__(self):
         self.host = '127.0.0.1'
-        self.port = '6397'
-        self.r = redis.StrictRedis(host=self.host, port=self.port)
+        self.port = 6379
+        self.r = redis.StrictRedis(host=self.host, port=self.port, db=0)
 
     def insertRedis(self, keyName, jsonStr):
         self.r.lpush(keyName, jsonStr)
@@ -17,7 +18,7 @@ def save1():
     someexpert = {'id': 1000, 'realname': 'buaa'}
     if RedisTT().r.exists('someexpert'):
         RedisTT().r.delete('someexpert')
-    RedisTT().insertRedis(keyName='someexpert', jsonStr=json.dump(someexpert))
+    RedisTT().insertRedis(keyName='someexpert', jsonStr=json.dumps(someexpert))
 
 
 if __name__ == "__main__":
